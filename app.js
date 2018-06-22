1f19b1f6b66111ffa34f8895de1833288841d084
connor ingold <connoringold@gmail.com>
2018-06-22 13:34:10 +0100

    im on a roll today

--- app.js
+++ app.js
@@ -3,42 +3,44 @@ const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 // storing the ul element in the ul varibale
 
+function createLI (text) {
+  const li = document.createElement('li');
+  // There are no li elements in the html so this method creates them and stores
+  // them in the li variable
+  li.textContent = text;
+  // this make it so the input from the user is turned in a li element
+  const label = document.createElement('label');
+  // creating a label
+  label.textContent = 'Confirmed';
+  // setting the text to Confirmed so on the page the text 'Confirmed will show'
+  const checkbox = document.createElement('input');
+  // creating an input element
+  checkbox.type = 'checkbox';
+  // the type of element we are creating is a checkbox
+  label.appendChild(checkbox);
+  // addding the label to the checkbox
+  li.appendChild(label);
+  // since the checkbox is now attached to the label we attach the label to the
+  // li and a checkbox will appear everytime a new li is created by the user
+  const button = document.createElement('button');
+  // creating an input element
+  button.textContent = 'remove';
+  // the type of element we are creating is a checkbox
+  li.appendChild(button);
+  // something
+  return li 
+}
+
 form.addEventListener('submit', e => {
 	// e is short for event this is know as the event parameter/argument
-	console.log('hi');
 	e.preventDefault();
-	// on the event of being sumbited we will prevent default behaviour
-	// were doing this because the default behaviour is refresh the page on submit
+	// when the user submits the form we cancel the browers usall submit behavior
 	const text = input.value;
-	// the users input is now stored in the above varibale
+	// store the input from the user
 	input.value = '';
-	// after the user input the name it will replace it with an empty string
-	// we can do becasue the suer input has been saved outside the function in
-	// the const input variable at the top of the page
-	const li = document.createElement('li');
-	// There are no li elements in the html so this method creates them and stores
-	// them in the li variable
-	li.textContent = text;
-	// this make it so the input from the user is turned in a li element
-	const label = document.createElement('label');
-	// creating a label
-	label.textContent = 'Confirmed';
-	// setting the text to Confirmed so on the page the text 'Confirmed will show'
-	const checkbox = document.createElement('input');
-	// creating an input element
-	checkbox.type = 'checkbox';
-	// the type of element we are creating is a checkbox
-	label.appendChild(checkbox);
-	// addding the label to the checkbox
-	li.appendChild(label);
-	// since the checkbox is now attached to the label we attach the label to the
-	// li and a checkbox will appear everytime a new li is created by the user
-  const buttom = document.createElement('button');
-	// creating an input element
-	button.textContent = 'remove';
-	// the type of element we are creating is a checkbox
-	label.appendChild(checkbox);
-  // something
+  // then clear the input
+  const li = createLI(text);
+
 	ul.appendChild(li);
 	// So far we have created the li element but we havent told them where to go
 	// so we make them as a child of the ul by the code and above and then they
@@ -56,5 +58,13 @@ ul.addEventListener('change', e => {
 		listItem.className = 'responded';
 	} else {
 		listItem.className = '';
+	}
+});
+
+ul.addEventListener('click', e => {
+	if (e.target.tagName === 'BUTTON') {
+    const li = e.target.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li)
 	}
 });
