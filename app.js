f6a54cb110eb7b497a35872e8123334781872cf0
connor ingold <connoringold@gmail.com>
2018-06-26 11:57:11 +0100

    finished and refactored the code

--- app.js
+++ app.js
@@ -1,114 +1,112 @@
-const form = document.getElementById('registar');
-const input = form.querySelector('input');
-const ul = document.getElementById('invitedList');
-// storing the ul element in the ul varibale
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('registar');
+  const input = form.querySelector('input');
 
-function createLI(text) {
-	const li = document.createElement('li');
-	const span = document.createElement('span');
-	// There are no li elements in the html so this method creates them and stores
-	// them in the li variable
-	span.textContent = text;
-	li.appendChild(span);
-	// this make it so the input from the user is turned in a li element
-	const label = document.createElement('label');
-	// creating a label
-	label.textContent = 'Confirmed';
-	// setting the text to Confirmed so on the page the text 'Confirmed will show'
-	const checkbox = document.createElement('input');
-	// creating an input element
-	checkbox.type = 'checkbox';
-	// the type of element we are creating is a checkbox
-	label.appendChild(checkbox);
-	// addding the label to the checkbox
-	li.appendChild(label);
-	// since the checkbox is now attached to the label we attach the label to the
-	// li and a checkbox will appear everytime a new li is created by the user
+  const mainDiv = document.querySelector('.main');
+  const ul = document.getElementById('invitedList');
 
-	const editButton = document.createElement('button');
-	// creating an input element
-	editButton.textContent = 'edit';
-	// the type of element we are creating is a checkbox
-	li.appendChild(editButton);
+  const div = document.createElement('div');
+  const filterLabel = document.createElement('label');
+  const filterCheckBox = document.createElement('input');
 
-	// const saveButton = document.createElement('button');
-	// // creating an input element
-	// saveButton.textContent = 'save';
-	// // the type of element we are creating is a checkbox
-	// li.appendChild(saveButton);
+  filterLabel.textContent = "Hide those who haven't responded";
+  filterCheckBox.type = 'checkbox';
+  div.appendChild(filterLabel);
+  div.appendChild(filterCheckBox);
+  mainDiv.insertBefore(div, ul);
+  filterCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if(isChecked) {
+      for (let i = 0; i < lis.length; i += 1) {
+        let li = lis[i];
+        if (li.className === 'responded') {
+          li.style.display = '';
+        } else {
+          li.style.display = 'none';
+        }
+      }
+    } else {
+      for (let i = 0; i < lis.length; i += 1) {
+        let li = lis[i];
+        li.style.display = '';
+      }
+    }
+  });
 
-	const removeButton = document.createElement('button');
-	// creating an input element
-	removeButton.textContent = 'remove';
-	// the type of element we are creating is a checkbox
-	li.appendChild(removeButton);
+  function createLI(text) {
+    function createElement(elementName, property, value) {
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
+    }
 
-	return li;
-}
+    function appendToLI(elementName, property, value) {
+      const element = createElement(elementName, property, value);
+      li.appendChild(element);
+      return element;
+    }
 
-form.addEventListener('submit', e => {
-	// e is short for event this is know as the event parameter/argument
-	e.preventDefault();
-	// when the user submits the form we cancel the browers usall submit behavior
-	const text = input.value;
-	// store the input from the user
-	input.value = '';
-	// then clear the input
-	const li = createLI(text);
+    const li = document.createElement('li');
+    appendToLI('span', 'textContent', text);
+    appendToLI('label', 'textContent', 'Confirmed')
+      .appendChild(createElement('input', 'type', 'checkbox'));
+    appendToLI('button', 'textContent', 'edit');
+    appendToLI('button', 'textContent', 'remove');
+    return li;
+  }
 
-	ul.appendChild(li);
-	// So far we have created the li element but we havent told them where to go
-	// so we make them as a child of the ul by the code and above and then they
-	// know who there parents (unlike me) and where they belong(on the webpage)
-	// on the webpage
-});
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
+    ul.appendChild(li);
+  });
 
-// function isEmpty() {
-//   const text = input.vaule
-//   if( text == '' ){
-//     document.write('Oops! looks the this field is empty')
-//   } else {
-//     document.write('Thats great! Anyone else? ')
-//   }
-// }
+  ul.addEventListener('change', (e) => {
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
 
-ul.addEventListener('change', e => {
-	//here were creating adding an event listener with the function of change
-	const checkbox = event.target;
-	const checked = checkbox.check;
-	const listItem = checkbox.parentNode.parentNode;
+    if (checked) {
+      listItem.className = 'responded';
+    } else {
+      listItem.className = '';
+    }
+  });
 
-	if (checked) {
-		listItem.className = 'responded';
-	} else {
-		listItem.className = '';
-	}
-});
+  ul.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const button = e.target;
+      const li = button.parentNode;
+      const ul = li.parentNode;
+      const action = button.textContent;
+      const nameActions = {
+        remove: () => {
+          ul.removeChild(li);
+        },
+        edit: () => {
+          const span = li.firstElementChild;
+          const input = document.createElement('input');
+          input.type = 'text';
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = 'save';
+        },
+        save: () => {
+          const input = li.firstElementChild;
+          const span = document.createElement('span');
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = 'edit';
+        }
+      };
 
-ul.addEventListener('click', e => {
-	if (e.target.tagName === 'BUTTON') {
-		const button = e.target;
-		const li = button.parentNode
-		const ul = li.parentNode
-		if (e.target.textContent === 'remove') {
-			const li = e.target.parentNode;
-			const ul = li.parentNode;
-			ul.removeChild(li);
-		} else if (button.textContent === 'edit') {
-			const span = li.firstElementChild;
-			const input = document.createElement('input');
-			input.type = 'text';
-			input.value = span.textContent;
-			li.insertBefore(input, span);
-			li.removeChild(span);
-			button.textContent = 'save';
-		} else if (button.textContent === 'save') {
-			const input = li.firstElementChild;
-			const span = document.createElement('span');
-			span.textContent = input.value;
-			li.insertBefore(span, input);
-			li.removeChild(input);
-			button.textContent = 'edit';
-		}
-	}
+      // select and run action in button's name
+      nameActions[action]();
+    }
+  });
 });
